ff7aab9cea8557f40677ca5c28a4216a4e10a4f2
Diego Rodriguez <diego.rodriguez@broadcom.com>
2024-11-10 12:08:49 +0100

    c1

---
+++ 2023/Day1P1.py
@@ -0,0 +1,19 @@
+# rexx - Advento of code Day 1-1 - 2023 
+
+# Abrimos el archivo en modo lectura
+with open('Day1_Input.txt', 'r') as archivo:
+    lineas = archivo.readlines()
+
+# Eliminamos el salto de línea (\n) al final de cada línea
+lineas = [linea.strip() for linea in lineas]
+
+tot = 0
+for linea in lineas:
+   # subTot=''
+   # for car in linea:
+   #    if car.isnumeric():
+   #       subTot=subTot + car
+   digits=[char for char in linea if char.isnumeric()]
+   tot1=int(digits[0]+digits[-1])
+   tot+=tot1
+print(tot)
